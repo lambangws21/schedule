7c404a252de221eaa6983ba98f07e073c94a5594
lambangws <macbookairm1@lambangws.local>
2025-03-26 00:21:20 +0700

    lexaa commit

--- src/app/doctors/page.tsx
+++ src/app/doctors/page.tsx
@@ -2,12 +2,11 @@
 import React from "react";
 import OperationForm from "@/components/doctors/DoctorForm";
 import StaffForm from "@/components/doctors/nurseForm";
-import OperationDataDisplay from "@/components/doctors/dataListCard";
+
 
 const Home: React.FC = () => {
   return (
     <div className="container mx-auto p-6">
-      <OperationDataDisplay />
       <OperationForm />
       <StaffForm />
     </div>
